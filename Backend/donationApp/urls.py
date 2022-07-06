d330ce2b940c1000ba6087a5f1c8d6c20c84b2fd
joycodes <jnguttu@gmail.com>
2022-07-06 04:07:44 +0300

    custom auth

--- Backend/donationApp/urls.py
+++ Backend/donationApp/urls.py
@@ -1,17 +1,29 @@
-from django.urls import path, include
+from django.urls import path,re_path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-        path('', views.index, name='index'),
-        path('About/', views.About, name='About'),
-        path('blog/', views.blog, name='blog'),
-        path('single-blog/', views.single_blog, name='single-blog'),
-        path('contact/', views.contact, name='contact'),
-        path('Cause/', views.cause, name='Cause'),
-        path('cause_details/', views.cause_details, name='cause_details'),
-        path('elements/', views.elements, name='elements'),
-        path('donate/', views.donate, name='donate'),
-
-
+  path('',views.home, name='donate-home'),
+  path('api/charities/',views.CharityList.as_view()),
+  path('api/donors/',views.DonorList.as_view()),
+  path('api/donations/',views.DonationsList.as_view()),
+  path('api/users/',views.UsersList.as_view()),
+  path('api/benefactor_stories/',views.BenefactorsList.as_view()),
+  path('api/beneficiaries/',views.BeneficiaryList.as_view()),
+  path('api/users/user-id/<int:pk>',views.UserDescription.as_view()),
+  path('api/donors/donors-id/<int:pk>',views.DonorDescription.as_view()),
+  path('api/donations/donations-id/<int:pk>',views.DonationsDescription.as_view()),
+  path('api/beneficiary/beneficiary-id/<int:pk>',views.BeneficiaryDescription.as_view()),
+  path('api/charities/charities-id/<int:pk>',views.CharityDescription.as_view()),
+  path('api/benefactor_stories/benefactor-id/<int:pk>',views.BenefactorDescription.as_view()),
+  path('api-token-auth/',obtain_auth_token),
+  path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+  path('api-auth', views.CustomAuthToken.as_view()),
+  
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- Backend/donationApp/views.py
+++ Backend/donationApp/views.py
@@ -1,32 +1,299 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Charity,Donor,Donations,CustomUser,BenefactorsStory,Beneficiaries
+from .serializer import CharitySerializer,DonorSerializer,DonationsSerializer,UsersSerializer,BenefactorSerializer,BeneficiarySerializer
+from rest_framework import status
+
+from rest_framework.parsers import MultiPartParser, JSONParser
+
+import cloudinary.uploader
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+
+
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
+def home(request):
+  return render(request, 'home.html')
 
-def About(request):
-    return render(request, 'About.html')
+# Donors
+class DonorList(APIView):
+  def get(self, request, format=None):
+    all_donors = Donor.objects.all()
+    serializers = DonorSerializer(all_donors,many=True)
+    return Response(serializers.data)
 
-def blog(request):
-    return render(request, 'blog.html')
+  def post(self, request, format=None):
+    serializers = DonorSerializer(data=request.data)
+    if serializers.is_valid():
+      serializers.save()
+      return Response(serializers.data,status=status.HTTP_201_CREATED)
+    return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
 
 
-def cause(request):
-    return render(request, 'cause.html')
+class DonorDescription(APIView):
+  def get_donor(self, pk):
+        try:
+            return Donor.objects.get(pk=pk)
+        except Donor.DoesNotExist:
+            return Http404
 
-def contact(request):
-    return render(request, 'contact.html')
+  def get(self, request, pk, format=None):
+      donor = self.get_donor(pk)
+      serializers = DonorSerializer(donor)
+      return Response(serializers.data)
 
-def single_blog(request):
-    return render(request, 'single_blog.html')
+  def put(self, request, pk, format=None):
+    donor = self.get_donor(pk)
+    serializers = DonorSerializer(donor, request.data)
+    if serializers.is_valid():
+        serializers.save()
+        return Response(serializers.data)
+    else:
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
-def elements(request):
-    return render(request, 'elements.html')
+  def delete(self, request, pk, format=None):
+    donor = self.get_donor(pk)
+    donor.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
 
-def cause_details(request):
-    return render(request, 'cause_details.html')
 
-def donate(request):
-    return render(request, 'donate.html')
+# Donations
+class DonationsList(APIView):
+  def get(self, request, format=None):
+    all_donations = Donations.objects.all()
+    serializers = DonationsSerializer(all_donations,many=True)
+    return Response(serializers.data)
 
+  def post(self, request, format=None):
+    serializers = DonationsSerializer(data=request.data)
+    if serializers.is_valid():
+      serializers.save()
+      return Response(serializers.data,status=status.HTTP_201_CREATED)
+    return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class DonationsDescription(APIView):
+  def get_donations(self, pk):
+        try:
+            return Donations.objects.get(pk=pk)
+        except Donations.DoesNotExist:
+            return Http404
+
+  def get(self, request, pk, format=None):
+      donations = self.get_donations(pk)
+      serializers = DonationsSerializer(donations)
+      return Response(serializers.data)
+
+  def put(self, request, pk, format=None):
+    donations = self.get_donations(pk)
+    serializers = DonationsSerializer(donations, request.data)
+    if serializers.is_valid():
+        serializers.save()
+        return Response(serializers.data)
+    else:
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def delete(self, request, pk, format=None):
+    donations = self.get_donations(pk)
+    donations.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# User
+class UsersList(APIView):
+  def get(self, request, format=None):
+    all_users = CustomUser.objects.all()
+    serializers = UsersSerializer(all_users,many=True)
+    return Response(serializers.data)
+
+  def post(self, request, format=None):
+    # request.data['user'] = user
+    serializers = UsersSerializer(data=request.data)
+    if serializers.is_valid():
+      serializers.save()
+      return Response(serializers.data,status=status.HTTP_201_CREATED)
+    return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class UserDescription(APIView):
+  def get_user(self, pk):
+    try:
+        return CustomUser.objects.get(pk=pk)
+    except CustomUser.DoesNotExist:
+        return Http404
+
+  def get(self, request, pk, format=None):
+    user = self.get_user(pk)
+    serializers = UsersSerializer(user)
+    return Response(serializers.data)
+
+  def put(self, request, pk, format=None):
+    user = self.get_user(pk)
+    serializers = UsersSerializer(user, request.data)
+    if serializers.is_valid():
+        serializers.save()
+        return Response(serializers.data)
+    else:
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def delete(self, request, pk, format=None):
+    user = self.get_user(pk)
+    user.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+# Charities
+class CharityList(APIView):
+  def get(self, request, format=None):
+    all_charities = Charity.objects.all()
+    serializers = CharitySerializer(all_charities,many=True)
+    return Response(serializers.data)
+  
+  def post(self, request, format=None):
+    serializers = CharitySerializer(data=request.data)
+    if serializers.is_valid():
+      serializers.save()
+      return Response(serializers.data,status=status.HTTP_201_CREATED)
+    return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class CharityDescription(APIView):
+  def get_charity(self, pk):
+        try:
+            return Charity.objects.get(pk=pk)
+        except Charity.DoesNotExist:
+            return Http404
+
+  def get(self, request, pk, format=None):
+      charity = self.get_charity(pk)
+      serializers = CharitySerializer(charity)
+      return Response(serializers.data)
+
+  def put(self, request, pk, format=None):
+    charity = self.get_charity(pk)
+    serializers = CharitySerializer(charity, request.data)
+    if serializers.is_valid():
+        serializers.save()
+        return Response(serializers.data)
+    else:
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def delete(self, request, pk, format=None):
+    charity = self.get_charity(pk)
+    charity.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+# beneficiaries
+class BenefactorsList(APIView):
+
+  def get(self, request, format=None):
+    all_benefactor = BenefactorsStory.objects.all()
+    serializers = BenefactorSerializer(all_benefactor,many=True)
+    return Response(serializers.data)
+  
+
+  def post(self, request):
+
+    serializers = BenefactorSerializer(data=request.data)
+    if serializers.is_valid():
+      serializers.save()
+      return Response(serializers.data,status=status.HTTP_201_CREATED)
+    return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+class BenefactorDescription(APIView):
+  def get_benefactor(self, pk):
+        try:
+            return BenefactorsStory.objects.get(pk=pk)
+        except BenefactorsStory.DoesNotExist:
+            return Http404
+
+  def get(self, request, pk, format=None):
+      benefactor = self.get_benefactor(pk)
+      serializers = BenefactorSerializer(benefactor)
+      return Response(serializers.data)
+
+  def put(self, request, pk, format=None):
+    benefactor = self.get_benefactor(pk)
+    serializers = BenefactorSerializer(benefactor, request.data)
+    if serializers.is_valid():
+        serializers.save()
+        return Response(serializers.data)
+    else:
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def delete(self, request, pk, format=None):
+    benefactor = self.get_benefactor(pk)
+    benefactor.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+# Beneficiary
+class BeneficiaryList(APIView):
+  # parser_classes = (
+  #       MultiPartParser,
+  #       JSONParser,
+  #   )
+  def get(self, request, format=None):
+    all_beneficiaries = Beneficiaries.objects.all()
+    serializers = BeneficiarySerializer(all_beneficiaries,many=True)
+    return Response(serializers.data)
+
+  def post(self, request, format=None):
+    # request.data['user'] = user
+    serializers = BeneficiarySerializer(data=request.data)
+    if serializers.is_valid():
+      serializers.save()
+      return Response(serializers.data,status=status.HTTP_201_CREATED)
+    return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+
+  # def post(self, request, format=None):
+  #    file = request.data.get('picture')
+  #    upload_data = cloudinary.uploader.upload(file)
+  #    return Response({
+  #           'status': 'success',
+  #           'data': upload_data,
+  #       }, status=201)
+
+class BeneficiaryDescription(APIView):
+  def get_beneficiary(self, pk):
+    try:
+        return Beneficiaries.objects.get(pk=pk)
+    except Beneficiaries.DoesNotExist:
+        return Http404
+
+  def get(self, request, pk, format=None):
+    beneficiary = self.get_beneficiary(pk)
+    serializers = BeneficiarySerializer(beneficiary)
+    return Response(serializers.data)
+
+  def put(self, request, pk, format=None):
+    beneficiary = self.get_beneficiary(pk)
+    serializers = BeneficiarySerializer(beneficiary, request.data)
+    if serializers.is_valid():
+        serializers.save()
+        return Response(serializers.data)
+    else:
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def delete(self, request, pk, format=None):
+    beneficiary = self.get_beneficiary(pk)
+    beneficiary.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+class CustomAuthToken(ObtainAuthToken):
+  def post(self, request, *args, **kwargs):
+    serializer = self.serializer_class(data=request.data,
+                                          context={'request': request})
+    serializer.is_valid(raise_exception=True)
+    user = serializer.validated_data['user']
+    token, created = Token.objects.get_or_create(user=user)
+    return Response({
+        'token': token.key,
+        'username': user.user_name,
+        'is_admin':user.is_admin,
+        'is_charity':user.is_charity,
+        'is_donor':user.is_donor,
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'user_id': user.pk,
+        'email': user.email
+      })     
