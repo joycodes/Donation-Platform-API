113133193d911954bf00c41e46c6f90d219990d9
joycodes <jnguttu@gmail.com>
2022-07-07 06:43:43 +0300

    add components

--- Frontend/Donation/src/app/app.module.ts
+++ Frontend/Donation/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutviewComponent } from './aboutview/aboutview.component';
+import { DonationviewComponent } from './donationview/donationview.component';
+import { HomeviewComponent } from './homeview/homeview.component';
+import { LoginviewComponent } from './loginview/loginview.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PaymentviewComponent } from './paymentview/paymentview.component';
+import { RegisterviewComponent } from './registerview/registerview.component';
+import { WorkviewComponent } from './workview/workview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutviewComponent,
+    DonationviewComponent,
+    HomeviewComponent,
+    LoginviewComponent,
+    NavbarComponent,
+    PaymentviewComponent,
+    RegisterviewComponent,
+    WorkviewComponent
   ],
   imports: [
     BrowserModule,
