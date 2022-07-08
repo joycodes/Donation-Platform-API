8024b04bb1264de91329a6d9fa660c21d16fc6ab
joycodes <jnguttu@gmail.com>
2022-07-08 04:45:02 +0300

    configure settings

--- donationProject/settings.py
+++ donationProject/settings.py
@@ -23,7 +23,7 @@ import cloudinary.api
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-MODE='prod'
+MODE='dev'
 
 
 # Quick-start development settings - unsuitable for production
