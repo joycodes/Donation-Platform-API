6f53de5fb1182587db4b5c032870b11a2fc64f7a
joycodes <jnguttu@gmail.com>
2022-07-08 05:12:26 +0300

    settings.py

--- donationProject/settings.py
+++ donationProject/settings.py
@@ -30,7 +30,7 @@ MODE='prod'
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default=1)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
